38f2a1e7dbc2e2a92c6b124a850b53ffdc8c5b05
XeroChan <maciej2013.mp@gmail.com>
2023-05-19 20:07:04 +0200

    init

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,4 +1,8 @@
 var searchData=
 [
-  ['game_0',['Game',['../class_game.html',1,'']]]
+  ['board_0',['Board',['../class_board.html',1,'']]],
+  ['board_1',['board',['../class_game.html#ae38e501e177586af48d2e2300b6be4ba',1,'Game']]],
+  ['board_2',['Board',['../class_board.html#acba1717762fb7fcc1fa7bc747eb0ca41',1,'Board::Board(int)'],['../class_board.html#a92d2482aebed23f380dcc5ecd93a5dfc',1,'Board::Board(const Board &amp;Computer)=delete'],['../class_board.html#a9ee491d4fea680cf69b033374a9fdfcb',1,'Board::Board()']]],
+  ['board_2ecpp_3',['Board.cpp',['../_board_8cpp.html',1,'']]],
+  ['board_2eh_4',['Board.h',['../_board_8h.html',1,'']]]
 ];
--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,18 +1,30 @@
 var indexSectionsWithContent =
 {
-  0: "bcghps",
-  1: "bcghps"
+  0: "_abcdghimopsv~",
+  1: "bcghps",
+  2: "bcgmps",
+  3: "bcghimopsv~",
+  4: "bis",
+  5: "_acdhps"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes"
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "defines"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes"
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Macros"
 };
 
