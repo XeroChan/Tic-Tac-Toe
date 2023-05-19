9f9679371f09ffde8be9dc0ecb6a323860b0628d
XeroChan <maciej2013.mp@gmail.com>
2023-05-19 20:37:33 +0200

    init

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,4 +1,10 @@
 var searchData=
 [
-  ['human_0',['Human',['../class_human.html',1,'']]]
+  ['c_5fversion_0',['C_VERSION',['../_c_make_c_compiler_id_8c.html#adaee3ee7c5a7a22451ea25e762e1d7d5',1,'CMakeCCompilerId.c']]],
+  ['checkwin_1',['checkWin',['../class_board.html#a4cdcfa3c443e8de85a10e12cf66e0edb',1,'Board']]],
+  ['cmakeccompilerid_2ec_2',['CMakeCCompilerId.c',['../_c_make_c_compiler_id_8c.html',1,'']]],
+  ['cmakecxxcompilerid_2ecpp_3',['CMakeCXXCompilerId.cpp',['../_c_make_c_x_x_compiler_id_8cpp.html',1,'']]],
+  ['compiler_5fid_4',['COMPILER_ID',['../_c_make_c_compiler_id_8c.html#a81dee0709ded976b2e0319239f72d174',1,'COMPILER_ID():&#160;CMakeCCompilerId.c'],['../_c_make_c_x_x_compiler_id_8cpp.html#a81dee0709ded976b2e0319239f72d174',1,'COMPILER_ID():&#160;CMakeCXXCompilerId.cpp']]],
+  ['computer_5',['Computer',['../class_computer.html',1,'Computer'],['../class_computer.html#a888944b91f4a1ee9875d0ac28e96a725',1,'Computer::Computer(char, Board &amp;)'],['../class_computer.html#af09dda36e4496b0bda1d23ecdc4922f3',1,'Computer::Computer(const Computer &amp;Computer)=delete']]],
+  ['cxx_5fstd_6',['CXX_STD',['../_c_make_c_x_x_compiler_id_8cpp.html#a34cc889e576a1ae6c84ae9e0a851ba21',1,'CMakeCXXCompilerId.cpp']]]
 ];
